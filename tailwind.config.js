ff9ee3d0f1d0cc9d3e903e198a44582130018600
Ronaldjga <Ronaldalmeida20022003@gmail.com>
2022-05-06 10:59:38 -0300

    config

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    './src/patterns/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        yellowPrimary: "#F2BF5E",
+        darkBluePrimary: "#022026",
+      },
+      fontFamily: {
+        sans: ['ui-sans-serif', 'system-ui',],
+        serif: ['ui-serif', 'Georgia',],
+        mono: ['ui-monospace', 'SFMono-Regular',],
+        h1Title: ['Cinzel',],
+        h2Title: ['"Domine"',],
+        body: ['"Work Sans"',],
+      }
+    },
+  },
+  plugins: [],
+}
